f731652aeed7a18a736afb48a627c7e42973a1ae
artemLink <artem.kislii.90@gmail.com>
2024-04-28 21:33:36 +0300

    Update

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+# Dockerfile
+
+FROM python:3.11
+
+ENV PYTHONUNBUFFERED 1
+ENV PYTHONDONTWRITEBYTECODE 1
+
+WORKDIR /code
+
+COPY requirements.txt /code/
+RUN pip install -r requirements.txt
+
+COPY . /code/
+
+# Сборка статических файлов
+RUN python manage.py collectstatic --noinput
--- boards/models.py
+++ boards/models.py
@@ -1,5 +1,10 @@
+import math
+
+from django.contrib.auth import get_user_model
 from django.db import models
-from django.contrib.auth.models import User
+from django.utils.safestring import mark_safe
+from django.utils.text import Truncator
+from markdown import markdown
 
 
 class Board(models.Model):
@@ -9,18 +14,58 @@ class Board(models.Model):
     def __str__(self):
         return self.name
 
+    def get_posts_count(self):
+        return self.topics.aggregate(models.Count('posts'))['posts__count']
+
+    def get_last_post(self):
+        latest_topic = self.topics.order_by('-last_updated').first()
+        if latest_topic:
+            latest_post = latest_topic.posts.order_by('-created_at').first()
+            return latest_post
+        else:
+            return None
+
 
 class Topic(models.Model):
     subject = models.CharField(max_length=255)
     last_updated = models.DateTimeField(auto_now_add=True)
-    board = models.ForeignKey(Board, related_name='topics', on_delete=models.CASCADE)
-    starter = models.ForeignKey(User, related_name='topics', on_delete=models.CASCADE)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='topics')
+    starter = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='topics')
+    views = models.PositiveIntegerField(default=0)
 
+    def __str__(self):
+        return self.subject
+
+    def get_page_count(self):
+        count = self.posts.count()
+        pages = count / 20
+        return math.ceil(pages)
+
+    def has_many_pages(self, count=None):
+        if count is None:
+            count = self.get_page_count()
+        return count > 6
+
+    def get_page_range(self):
+        count = self.get_page_count()
+        if self.has_many_pages(count):
+            return range(1, 5)
+        return range(1, count + 1)
+
+    def get_last_ten_posts(self):
+        return self.posts.order_by('-created_at')[:10]
 
 class Post(models.Model):
     message = models.TextField(max_length=4000)
-    topic = models.ForeignKey(Topic, related_name='posts', on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, related_name='posts')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True)
-    created_by = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
-    updated_by = models.ForeignKey(User, null=True, related_name='+', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='posts')
+    updated_by = models.ForeignKey(get_user_model(), null=True, on_delete=models.CASCADE, related_name='+')
+
+    def __str__(self):
+        truncated_message = Truncator(self.message)
+        return truncated_message.chars(30)
+
+    def get_message_as_markdown(self):
+        return mark_safe(markdown(self.message, safe_mode='escape'))
